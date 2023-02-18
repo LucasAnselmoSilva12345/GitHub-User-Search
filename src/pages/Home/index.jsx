c66a675848fac1d1444fe89a5036dd8f6dfa8d07
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-02-17 21:00:46 -0300

    Feat Card Details GitHub User component import in Homepage
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import { Header } from '../../components/Header';
 import { InputSearch } from '../../components/InputSearch';
 import '../../styles/global.css';
 import { SectionContainer } from './style.js';
+import { CardDetailsGitHubUser } from '../../components/CardDetailsGitHubUser';
 
 export function Home() {
   return (
@@ -9,6 +10,7 @@ export function Home() {
       <Header />
       <SectionContainer>
         <InputSearch />
+        <CardDetailsGitHubUser />
       </SectionContainer>
     </>
   );
