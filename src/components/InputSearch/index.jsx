43db64188e6deacc51a86f7d95261808c62043ea
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-02-22 12:32:45 -0300

    Feat values that are requested from the GitHub API
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/InputSearch/index.jsx
+++ src/components/InputSearch/index.jsx
@@ -1,39 +1,62 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { CardProfileGitHub } from 'lulib-ui';
 
-import { Container, Input, Button } from './style.js';
+import {
+  Container,
+  HeaderSearch,
+  Input,
+  Button,
+  WithoutResults,
+} from './style.js';
+import { CardDetailsGitHubUser } from '../CardDetailsGitHubUser/index';
 
 export function InputSearch() {
   const [githubUsername, setGitHubUsername] = useState('');
+  const [githubName, setGitHubName] = useState('');
+  const [githubBio, setGitHubBio] = useState('');
+  const [githubAvatarURL, setGitHubAvatarURL] = useState('');
+  const [githubLocation, setGitHubLocation] = useState('');
+  const [githubURL, setGitHubURL] = useState('');
 
   function handleSearchGitHubUser(event) {
     event.preventDefault();
-
     axios
       .get(`https://api.github.com/users/${githubUsername}`)
       .then((response) => {
-        return response.data;
+        setGitHubName(response.data.name);
+        setGitHubBio(response.data.bio);
+        setGitHubAvatarURL(response.data.avatar_url);
+        setGitHubLocation(response.data.location);
+        setGitHubURL(response.data.url);
       });
   }
 
   return (
     <Container>
-      <form>
+      <HeaderSearch>
         <Input
           type="text"
-          value={githubUsername}
-          onChange={(event) => setGitHubUsername(event.target.value)}
+          onChange={(event) => {
+            setGitHubUsername(event.target.value);
+          }}
           placeholder="Digite o username do GitHub"
         />
-        <Button>Search</Button>
-      </form>
+        <Button onClick={handleSearchGitHubUser}>Search</Button>
+      </HeaderSearch>
 
-      {githubUsername.length === 0 ? (
-        <p>Nenhum usuário pesquisado</p>
-      ) : (
-        <CardProfileGitHub username={githubUsername} />
-      )}
+      <section>
+        {githubName ? (
+          <CardDetailsGitHubUser
+            avatar_url={githubAvatarURL}
+            name={githubName}
+            bio={githubBio}
+            location={githubLocation}
+            url={githubURL}
+          />
+        ) : (
+          <WithoutResults>Nenhum usuário pesquisado...</WithoutResults>
+        )}
+      </section>
     </Container>
   );
 }
--- src/components/InputSearch/style.js
+++ src/components/InputSearch/style.js
@@ -4,10 +4,9 @@ export const Container = styled.div`
   width: 100%;
   padding: 1rem;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-between;
-  gap: 0.5rem;
+  gap: 3rem;
 
   @media (min-width: 992px) {
     width: 70%;
@@ -16,13 +15,19 @@ export const Container = styled.div`
   }
 `;
 
-export const Form = styled.form`
-  width: 100%;
+export const HeaderSearch = styled.div`
+  width: 80%;
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: space-between;
-  gap: 0.5rem;
+  justify-content: center;
+  gap: 1rem;
+
+  @media (min-width: 992px) {
+    width: 100%;
+    margin: 0 auto;
+    justify-content: space-between;
+  }
 `;
 
 export const Input = styled.input`
@@ -65,4 +70,11 @@ export const Button = styled.button`
   &:hover {
     background-color: #4b5563;
   }
+`;
+
+export const WithoutResults = styled.p`
+  font-size: 1rem;
+  font-weight: 300;
+  text-align: center;
+  color: #333;
 `;
