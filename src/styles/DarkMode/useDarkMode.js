d8f3a56b6969464958c0baeaa73f6f2b571a5164
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-02-23 18:22:36 -0300

    Set value of theme in Local Storage
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/styles/DarkMode/useDarkMode.js
+++ src/styles/DarkMode/useDarkMode.js
@@ -1,11 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const useDarkMode = () => {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState('light');
+
+  const setMode = (mode) => {
+    window.localStorage.setItem('theme', mode);
+    setTheme(mode);
+  };
 
   const toggleTheme = () => {
-    theme === 'dark' ? setTheme('light') : setTheme('dark');
+    theme === 'dark' ? setMode('light') : setMode('dark');
   };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem('theme');
+    localTheme ? setTheme(localTheme) : setMode('dark');
+  }, []);
 
   return [theme, toggleTheme];
 };
